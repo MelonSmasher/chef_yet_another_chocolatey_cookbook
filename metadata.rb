fed2ba6b022149354bec61a87092c2f1be95e689
Alex Markessinis <markea@sage.edu>
2017-02-01 00:33:41 -0500

    everything working

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'markea125@gmail.com'
 license 'MIT'
 description 'YACC (Yet Another Chocolatey Cookbook), Manages many chocolatey packages through parameters.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.3'
 supports 'windows'
 issues_url 'https://github.com/MelonSmasher/yacc/issues'
 source_url 'https://github.com/MelonSmasher/yacc'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 # This function calls the upstream chocolatey resource built into chefs
-def run_upstream(action, options, source, ignore_failure)
+def run_upstream(package, action, options, source, ignore_failure)
   chocolatey_package package do
     options options
     source source
@@ -18,13 +18,13 @@ source = node['yacc']['source']
 # Global value for ignoring failures
 ignore_failure = node['yacc']['ignore_failure']
 # Grab install options that will be applied to each package
-install_options =''
+install_options = ''
 if not node['yacc']['install_options'].empty?
   # Are the install options an array?
-  if node['yacc']['install_options'].is_a?
+  if node['yacc']['install_options'].kind_of?(Array)
     # Strip each array element then join array to a single string
     install_options = node['yacc']['install_options'].each { |a| a.strip! if a.respond_to? :strip! }.join(' ')
-  elsif node['yacc']['install_options'].is_s?
+  elsif node['yacc']['install_options'].kind_of?(String)
     install_options = node['yacc']['install_options'].strip
   else
     log 'YACC Global' do
@@ -41,17 +41,19 @@ node['yacc']['packages'].each do |package, package_options|
   # Is the source overridden for this package?
   source = package_options['source'] if not package_options['action'].empty?
   # If there are any package specific install options append them to the global install options
-  if not package_options['install_options'].empty?
-    # Check to see if it's an array or string
-    if package_options['install_options'].is_a?
-      # Strip each array element then join array to a single string
-      install_options = package_options['install_options'].each { |a| a.strip! if a.respond_to? :strip! }.join(' ') + ' ' + install_options
-    elsif package_options['install_options'].is_s?
-      install_options = package_options['install_options'].strip + ' ' + install_options
-    else
-      log 'YACC Package' do
-        message "The package: '#{package}' contains a malformed install option, ignoring..."
-        level :warn
+  if package_options.key?('install_options')
+    if not package_options['install_options'].empty?
+      # Check to see if it's an array or string
+      if package_options['install_options'].kind_of?(Array)
+        # Strip each array element then join array to a single string
+        install_options = package_options['install_options'].each { |a| a.strip! if a.respond_to? :strip! }.join(' ') + ' ' + install_options
+      elsif package_options['install_options'].kind_of?(String)
+        install_options = package_options['install_options'].strip + ' ' + install_options
+      else
+        log 'YACC Package' do
+          message "The package: '#{package}' contains a malformed install option, ignoring..."
+          level :warn
+        end
       end
     end
 
@@ -59,17 +61,17 @@ node['yacc']['packages'].each do |package, package_options|
   # Switch over the various actions and pass in the correct action symbol
   case action_option
     when 'install'
-      run_upstream(:install, install_options, source, ignore_failure)
+      run_upstream(package, :install, install_options, source, ignore_failure)
     when 'purge'
-      run_upstream(:purge, install_options, source, ignore_failure)
+      run_upstream(package, :purge, install_options, source, ignore_failure)
     when 'reconfig'
-      run_upstream(:reconfig, install_options, source, ignore_failure)
+      run_upstream(package, :reconfig, install_options, source, ignore_failure)
     when 'remove'
-      run_upstream(:remove, install_options, source, ignore_failure)
+      run_upstream(package, :remove, install_options, source, ignore_failure)
     when 'uninstall'
-      run_upstream(:uninstall, install_options, source, ignore_failure)
+      run_upstream(package, :uninstall, install_options, source, ignore_failure)
     when 'upgrade'
-      run_upstream(:upgrade, install_options, source, ignore_failure)
+      run_upstream(package, :upgrade, install_options, source, ignore_failure)
     else # If we make it here, try the action as a version number.
       chocolatey_package package do
         version action_option
